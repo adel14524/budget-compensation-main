8b48cbf6ff423f3472937e2c763760d7c11fdbed
adel14524 <nuradilah778@gmail.com>
2021-12-22 22:44:00 +0800

    Update revenue.php

--- class/revenue.php
+++ class/revenue.php
@@ -31,7 +31,19 @@ class Revenue{
 		}
 	}
 
+	public function addCostLog($fields = array()){
+		if(!$this->_db->insert('revenuelog', $fields)) {
+			throw new Exception('There was a problem adding data.');
+		}
+	}
+
 	public function updateRevenue($fields = array(), $id = null, $idname = null){
+		if (!$this->_db->update('budget_revenue', $id, $fields, $idname)) {
+			throw new Exception('There was a problem updating activity.');
+		}
+	}
+
+	public function updateCost($fields = array(), $id = null, $idname = null){
 		if (!$this->_db->update('budget_revenue', $id, $fields, $idname)) {
 			throw new Exception('There was a problem updating activity.');
 		}
@@ -77,6 +89,16 @@ class Revenue{
 			}
 		}
 	}
+	public function searchCostOfGoodSoldLog($budgetRevenueID =null){
+		if($budgetRevenueID){
+			$data = $this->_db->get('revenuelog', array("budgetRevenueID", '=', $budgetRevenueID));
+			// $field = (is_numeric($revenueLogID)) ? 'revenueLogID' : 'revenuelog';
+			if($data->count()){
+				$this->_data = $data->results();
+				return $this->_data;
+			}
+		}
+	}
 
 	public function searchRevenueestimate($value = null, $year =null, $typeRevenue=null){
 		if($value){
@@ -88,6 +110,15 @@ class Revenue{
 		}
 	}	
 	public function searchRevenueactual($value = null, $year =null, $typeRevenue= null){
+		if($value){
+			$data = $this->_db->get2('budget_revenue', array("companyID", '=', $value), array("year", '=', $year), array("typeRevenue", '=', $typeRevenue));
+			if($data->count()){
+				$this->_data = $data->first();
+				return $this->_data;
+			}
+		}
+	}
+	public function searchCostOfGoodSold($value = null, $year =null, $typeRevenue= null){
 		if($value){
 			$data = $this->_db->get2('budget_revenue', array("companyID", '=', $value), array("year", '=', $year), array("typeRevenue", '=', $typeRevenue));
 			if($data->count()){
